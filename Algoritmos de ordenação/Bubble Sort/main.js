0e1d1564ba081947ec04dbb58ecc5c76bc9e19f6
Kaio <kaioramos990@Hotmail.com>
2025-06-18 08:56:57 -0300

    feat: bubble sort

---
+++ Algoritmos de ordenação/Bubble Sort/main.js
@@ -0,0 +1,18 @@
+function bubbleSort(arr){
+    
+    let aux = 0;
+
+    for(let lastElement = arr.length - 1; lastElement > 0; lastElement--){
+        for(let i = 0; i < lastElement; i++){
+            if(arr[i] > arr[i+1]){
+                aux = arr[i];
+                arr[i] = arr[i + 1]; 
+                arr[i + 1] = aux;
+            }
+        }
+    }
+
+    return arr;
+}
+
+console.log(bubbleSort([3, 8, 9, 4, 5,7, 2, 35, 28, 13]));
